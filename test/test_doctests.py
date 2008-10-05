fe648850f5cb246ed7268fb0d33d53d5a6f48f14
tiftof <tiftof@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-05 16:23:41 +0000

    IDEA, RC5 handling by doctest script
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@36 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- test/test_doctests.py
+++ test/test_doctests.py
@@ -3,12 +3,21 @@
 import unittest
 import doctest
 #import CryptoPlus.Cipher.python_AES 
-from CryptoPlus.Cipher import python_AES, AES, python_DES, DES, python_DES3, DES3, python_Blowfish, Blowfish, python_Twofish, python_Serpent, python_Rijndael, CAST, ARC2, IDEA, RC5
+from CryptoPlus.Cipher import python_AES, AES, python_DES, DES, python_DES3, DES3, python_Blowfish, Blowfish, python_Twofish, python_Serpent, python_Rijndael, CAST, ARC2
+try:
+        from CryptoPlus.Cipher import IDEA
+        from CryptoPlus.Cipher import RC5
+        import_error = 0
+except ImportError:
+        import_error = 1
 
 suite = unittest.TestSuite()
 #for mod in (CryptoPlus.Cipher.python_AES,CryptoPlus.Cipher.python_AES):
-for mod in python_AES, AES, python_DES, DES, python_DES3, DES3, python_Blowfish, Blowfish, python_Twofish, python_Serpent, python_Rijndael, CAST, ARC2, IDEA, RC5:
+for mod in python_AES, AES, python_DES, DES, python_DES3, DES3, python_Blowfish, Blowfish, python_Twofish, python_Serpent, python_Rijndael, CAST, ARC2:
     suite.addTest(doctest.DocTestSuite(mod))
+if not import_error:
+    suite.addTest(doctest.DocTestSuite(IDEA))
+    suite.addTest(doctest.DocTestSuite(RC5))
 runner = unittest.TextTestRunner()
 runner.run(suite)
 
