7d3019617a6aed8ba0175c25c21c97bc73ab5d05
tiftof <tiftof@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-05 16:23:40 +0000

    adjusted IDEA, RC5 handling
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@35 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- src/Cipher/__init__.py
+++ src/Cipher/__init__.py
@@ -1,2 +1,10 @@
 from Crypto.Cipher import ARC4, XOR
-__all__ = ["AES","python_AES","python_DES","python_DES3","DES","DES3","Blowfish","python_Blowfish","python_Twofish","python_Serpent","python_Rijndael","ARC4","ARC2","CAST","XOR","IDEA","RC5"]
+
+__all__ = ["AES","python_AES","python_DES","python_DES3","DES","DES3","Blowfish","python_Blowfish","python_Twofish","python_Serpent","python_Rijndael","ARC4","ARC2","CAST","XOR"]
+
+try:
+        import Crypto.Cipher.IDEA
+        __all__.append("IDEA")
+        __all__.append("RC5")
+except ImportError:
+        pass
