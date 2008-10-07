994c436c5335025ab71f040b41fe7bd902d25258
tiftof <tiftof@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-07 13:40:14 +0000

    compatibility with pycrypto 2.0.1 and earlier for python_compat import
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@59 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- src/Util/python_compat.py
+++ src/Util/python_compat.py
@@ -1,1 +1,6 @@
-from Crypto.Util.python_compat import *
+from pkg_resources import parse_version
+import Crypto
+
+if parse_version(Crypto.__version__) > parse_version("2.0.1"):
+        del Crypto
+        from Crypto.Util.python_compat import *
