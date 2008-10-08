2471bf323685303887db74f03273d75272bfd086
phil <phil@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-08 12:25:42 +0000

    mv src/* to src/CryptoPlus/* & make setup.py more generic
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@63 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='CryptoPlus',
       version='1.0',
       description='PyCrypto Cipher extension',
       author='Christophe Oosterlynck',
       author_email='tiftof@gmail.com',
-      packages = ["CryptoPlus","CryptoPlus.Cipher", "CryptoPlus.Util","CryptoPlus.SelfTest","CryptoPlus.Random","CryptoPlus.SelfTest.Hash","CryptoPlus.SelfTest.Cipher","CryptoPlus.SelfTest.Random","CryptoPlus.SelfTest.Util","CryptoPlus.SelfTest.PublicKey"],
+      packages = find_packages('src'),
       install_requires = ['pycrypto'],
-      package_dir = {'CryptoPlus': 'src'}
+      package_dir={'': 'src'}
      )
 
