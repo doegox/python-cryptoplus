208ca7bde35dfbd25c29eedd2e186ad58b1f7403
phil <phil@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-08 12:47:29 +0000

    test.py (dict) was shadowing builtin dict()
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@65 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- test/test.py
+++ test/test.py
@@ -129,11 +129,11 @@ for i in range(0,len(dict_des)/3):
 
 from CryptoPlus.Cipher import python_DES3
 
-for dict in dict_tdes2,dict_tdes3:
-    for i in range(0,len(dict)/3):
-        msg = dict['msg%i'%i].decode('hex')
-        key = dict['key%i'%i].decode('hex')
-        cip = dict['cip%i'%i].decode('hex')
+for d in dict_tdes2,dict_tdes3:
+    for i in range(0,len(d)/3):
+        msg = d['msg%i'%i].decode('hex')
+        key = d['key%i'%i].decode('hex')
+        cip = d['cip%i'%i].decode('hex')
         cipher = python_DES3.new(key,python_DES3.MODE_ECB)
         if cip <> cipher.encrypt(msg):
             print 'ERROR! for TDES2/3 in %i'%i
@@ -143,11 +143,11 @@ print "Serpent"
 
 from CryptoPlus.Cipher import python_Serpent
 
-for dict in dict_serpent128,dict_serpent192,dict_serpent256:
-    for i in range(0,len(dict)/3):
-        msg = dict['msg%i'%i].decode('hex')
-        key = dict['key%i'%i].decode('hex')
-        cip = dict['cip%i'%i].decode('hex')
+for d in dict_serpent128,dict_serpent192,dict_serpent256:
+    for i in range(0,len(d)/3):
+        msg = d['msg%i'%i].decode('hex')
+        key = d['key%i'%i].decode('hex')
+        cip = d['cip%i'%i].decode('hex')
         cipher = python_Serpent.new(key,python_Serpent.MODE_ECB)
         if cip <> cipher.encrypt(msg):
             print 'ERROR! for Serpent in %i'%i
@@ -157,30 +157,30 @@ print "CMAC-AES"
 
 from CryptoPlus.Cipher import python_AES
 
-for dict in dict_cmac_aes128,dict_cmac_aes192,dict_cmac_aes256:
-    for i in range(0,len(dict)/4):
-        msg = dict['msg%i'%i].decode('hex')
-        key = dict['key%i'%i].decode('hex')
+for d in dict_cmac_aes128,dict_cmac_aes192,dict_cmac_aes256:
+    for i in range(0,len(d)/4):
+        msg = d['msg%i'%i].decode('hex')
+        key = d['key%i'%i].decode('hex')
         if msg == '\x00':
             msg = ''
-        mac = dict['mac%i'%i].decode('hex')
+        mac = d['mac%i'%i].decode('hex')
         cipher = python_AES.new(key,python_AES.MODE_CMAC)
-        if mac <> cipher.encrypt(msg)[:dict['taglength%i'%i]]:
+        if mac <> cipher.encrypt(msg)[:d['taglength%i'%i]]:
             print 'ERROR for %i'%i
 
 # CMAC-TDES2/3
 print "CMAC-TDES"
 from CryptoPlus.Cipher import python_DES3
 
-for dict in dict_cmac_tdes2,dict_cmac_tdes3:
-    for i in range(0,len(dict)/4):
-        msg = dict['msg%i'%i].decode('hex')
+for d in dict_cmac_tdes2,dict_cmac_tdes3:
+    for i in range(0,len(d)/4):
+        msg = d['msg%i'%i].decode('hex')
         if msg == '\x00':
             msg = ''
-        key = dict['key%i'%i].decode('hex')
-        mac = dict['mac%i'%i].decode('hex')
+        key = d['key%i'%i].decode('hex')
+        mac = d['mac%i'%i].decode('hex')
         cipher = python_DES3.new(key,python_DES3.MODE_CMAC)
-        if mac <> cipher.encrypt(msg)[:dict['taglength%i'%i]]:
+        if mac <> cipher.encrypt(msg)[:d['taglength%i'%i]]:
             print 'ERROR! on %i'%i
 
 # XTS-AES
