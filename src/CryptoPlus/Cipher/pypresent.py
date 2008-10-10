c98e94c10b9cc3837bd6ca43a78e27755cf80414
tiftof <tiftof@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-10 15:01:38 +0000

    PRESENT: finished switching all internal representations to integers
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@85 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- src/CryptoPlus/Cipher/pypresent.py
+++ src/CryptoPlus/Cipher/pypresent.py
@@ -23,13 +23,13 @@ class Present:
                 Supply the plaintext block as a raw string and the raw
                 ciphertext will be returned.
                 """
-                state = block.encode('hex')
+                state = string2number(block)
                 for i in range (1,self.rounds):
                         state = addRoundKey(state,self.roundkeys[i-1])
                         state = sBoxLayer(state)
                         state = pLayer(state)
                 cipher = addRoundKey(state,self.roundkeys[self.rounds-1])
-                return cipher.decode('hex')
+                return number2string_N(cipher,8)
 
         def decrypt(self,block):
                 """Decrypting 1 block (8 bytes)
@@ -37,20 +37,19 @@ class Present:
                 Supply the ciphertext block as a raw string and the raw
                 plaintext will be returned.
                 """
-                state = block.encode('hex')
+                state = string2number(block)
                 for i in range (1,self.rounds):
                         state = addRoundKey(state,self.roundkeys[self.rounds-i])
                         state = pLayer_dec(state)
                         state = sBoxLayer_dec(state)
                 decipher = addRoundKey(state,self.roundkeys[0])
-                return decipher.decode('hex')
+                return number2string_N(decipher,8)
 
         def get_block_size(self):
                 return 8
 
 #        0   1   2   3   4   5   6   7   8   9   a   b   c   d   e   f
-SBox = ['c','5','6','b','9','0','a','d','3','e','f','8','4','7','1','2']
-SBox2= [0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2]
+Sbox= [0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2]
 PBox = [0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
         4,20,36,52,5,21,37,53,6,22,38,54,7,23,39,55,
         8,24,40,56,9,25,41,57,10,26,42,58,11,27,43,59,
@@ -64,13 +63,13 @@ def generateRoundkeys80(key,rounds):
         for i in range(1,rounds+1): # (K1 ... K32)
                 # rawkey: used in comments to show what happens at bitlevel
                 # rawKey[0:64]
-                roundkeys.append(("%x" % (key >>16 )).zfill(64/4))
+                roundkeys.append(key >>16)
                 #1. Shift
                 #rawKey[19:len(rawKey)]+rawKey[0:19]
                 key = ((key & (2**19-1)) << 61) + (key >> 19)
                 #2. SBox
                 #rawKey[76:80] = S(rawKey[76:80])
-                key = (SBox2[key >> 76] << 76)+(key & (2**76-1))
+                key = (Sbox[key >> 76] << 76)+(key & (2**76-1))
                 #3. Salt
                 #rawKey[15:20] ^ i
                 key ^= (i & (2**5-1)) << 15
@@ -83,56 +82,55 @@ def generateRoundkeys128(key,rounds):
         roundkeys = []
         for i in range(1,rounds+1): # (K1 ... K32)
                 # rawkey: used in comments to show what happens at bitlevel
-                roundkeys.append(("%x" % (key >>64)).zfill(64/4))
+                roundkeys.append(key >>64)
                 #1. Shift
                 key = ((key & (2**67-1)) << 61) + (key >> 67)
                 #2. SBox
-                key = (SBox2[key >> 124] << 124)+(SBox2[(key >> 120) & 0xF] << 120)+(key & (2**120-1))
+                key = (Sbox[key >> 124] << 124)+(Sbox[(key >> 120) & 0xF] << 120)+(key & (2**120-1))
                 #3. Salt
                 #rawKey[62:67] ^ i
                 key ^= (i & (2**5-1)) << 62
         return roundkeys
 
 def addRoundKey(state,roundkey):
-        return ( "%x" % ( int(state,16) ^ int(roundkey,16) ) ).zfill(16)
+        return state ^ roundkey
 
 def sBoxLayer(state):
         """SBox function for encryption
 
         Takes a hex string as input and will output a hex string"""
-        output =''
-        for i in range(len(state)):
-                output += SBox[int(state[i],16)]
+
+        output = 0
+        for i in range(0,16):
+                output += Sbox[( state >> (i*4)) & 0xF] << (i*4)
         return output
 
 def sBoxLayer_dec(state):
         """Inverse SBox function for decryption
 
         Takes a hex string as input and will output a hex string"""
-        output =''
-        for i in range(len(state)):
-                output += hex( SBox.index(state[i]) )[2:]
+        output = 0
+        for i in range(0,16):
+                output += Sbox.index(( state >> (i*4)) & 0xF) << (i*4)
         return output
 
 def pLayer(state):
         """Permutation layer for encryption
 
         Takes a 64bit hex string as input and will output a 64bit hex string"""
-        output = ''
-        state_bin = bin(int(state,16)).zfill(64)[::-1][0:64]
+        output = 0
         for i in range(64):
-                output += state_bin[PBox.index(i)]
-        return ("%x" % int(output[::-1],2)).zfill(16)
+                output += ((state >> i) & 0x01) << PBox[i]
+        return output
 
 def pLayer_dec(state):
         """Permutation layer for decryption
 
         Takes a 64bit hex string as input and will output a 64bit hex string"""
-        output = ''
-        state_bin = bin(int(state,16)).zfill(64)[::-1][0:64]
+        output = 0
         for i in range(64):
-                output += state_bin[PBox[i]]
-        return ("%x" % int(output[::-1],2)).zfill(16)
+                output += ((state >> i) & 0x01) << PBox.index(i)
+        return output
 
 def bin(a):
         """Convert an integer to a bin string (1 char represents 1 bit)"""
@@ -150,3 +148,13 @@ def string2number(i):
     Output: long or integer
     """
     return int(i.encode('hex'),16)
+
+def number2string_N(i, N):
+    """Convert a number to a string of fixed size
+
+    i: long or integer
+    N: length of string
+    Output: string (big-endian)
+    """
+    s = '%0*x' % (N*2, i)
+    return s.decode('hex')
