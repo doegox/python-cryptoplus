1a629fb87da0fa53dd50f630117c2256058c4621
tiftof <tiftof@49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1>
2008-10-10 15:01:39 +0000

    PRESENT: inverse PBox is calculated on import -> speed gain
    using the "index()" method on PBox all the time in pLayer_dec was
    slowing things down
    
    git-svn-id: svn+ssh://devel.yobi.be/home/svn/CryptoPlus/trunk@86 49921240-e1b6-4d8d-af3b-5ae6c3d9e7c1

--- src/CryptoPlus/Cipher/pypresent.py
+++ src/CryptoPlus/Cipher/pypresent.py
@@ -54,6 +54,8 @@ PBox = [0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
         4,20,36,52,5,21,37,53,6,22,38,54,7,23,39,55,
         8,24,40,56,9,25,41,57,10,26,42,58,11,27,43,59,
         12,28,44,60,13,29,45,61,14,30,46,62,15,31,47,63]
+PBox_inv = []
+PBox_inv.extend(PBox.index(x) for x in range(64))
 
 def generateRoundkeys80(key,rounds):
         """Generate the roundkeys for a 80 bit key
@@ -129,7 +131,7 @@ def pLayer_dec(state):
         Takes a 64bit hex string as input and will output a 64bit hex string"""
         output = 0
         for i in range(64):
-                output += ((state >> i) & 0x01) << PBox.index(i)
+                output += ((state >> i) & 0x01) << PBox_inv[i]
         return output
 
 def bin(a):
