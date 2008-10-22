40dc9f4cb1797c379a0e607c3a0b274a294129b2
Christophe Oosterlynck <christophe@christophe-desktop.(none)>
2008-10-22 18:25:36 +0200

    RadioGatun: first version

---
+++ src/CryptoPlus/Hash/python_RadioGatun.py
@@ -0,0 +1,13 @@
+from pyradiogatun import new
+
+__all__ = ['new']
+
+#def new(wldata=""):
+#        """Return a new RadioGatun hash object
+#
+#        wl  = wordlength (in bits) of the RadioGatun hash method
+#               between 1 and 64 (default = 64)
+#        arg =  if present, the method call update(arg) is made
+#        """
+#        return pyradiogatun.new(data)
+
